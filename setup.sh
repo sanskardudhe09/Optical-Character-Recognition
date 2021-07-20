a4fda77fcce2fdc997174b0fe084db8a8a822ecb
sanskardudhe09 <69566078+sanskardudhe09@users.noreply.github.com>
2021-07-20 17:44:33 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
+
+web: sh setup.sh && streamlit run app.py
